ab1c9141134666f81671dba27d80b88c750c0c14
Junaidk007 <junaid.k0004@gmail.com>
2026-05-21 15:53:31 +0530

    updated the multer config

--- backend/config.js
+++ backend/config.js
@@ -1,8 +1,12 @@
 const multer = require('multer');
+const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, path.join(
+        __dirname,
+        "uploads"
+      ));
   },
   filename: (req, file, cb) => {
     // const ext = path.extname(file.originalname); // 👈 get extension
